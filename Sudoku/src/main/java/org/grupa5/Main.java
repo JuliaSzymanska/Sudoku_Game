4077672897e89076b9e351290d5f08710c2a82b9
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2020-03-11 12:25:48 +0100

    init

---
+++ Sudoku/src/main/java/org/grupa5/Main.java
@@ -0,0 +1,7 @@
+package org.grupa5;
+
+class Main {
+    public static void main(final String[] args) {
+        // : )
+    }
+}
---
+++ Sudoku/src/test/java/org/grupa5/ExampleTest.java
@@ -0,0 +1,14 @@
+package org.grupa5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest{
+
+    @Test
+    public void test_triangle(){
+            assertTrue(true);
+        }
+}
