477f5499f5ac01a52d21f22152915a81199b3aa0
224466 Zdrzalik Przemyslaw <224466@edu.p.lodz.pl>
2020-03-26 00:02:53 +0100

    finished zadanie 5

--- Sudoku/src/main/java/org/grupa5/sudoku/BacktrackingSudokuSolver.java
+++ Sudoku/src/main/java/org/grupa5/sudoku/BacktrackingSudokuSolver.java
@@ -1,6 +1,10 @@
 package org.grupa5.sudoku;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BacktrackingSudokuSolver implements SudokuSolver {
 
@@ -13,11 +17,7 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
 
     @Override
     public void solve(SudokuBoard board) {
-        for (int i = 0; i <= 8; i++) {
-            this.randomFillSector(i, board);
-        }
         this.solveSudoku(board);
-        this.mixBoard(board);
     }
 
     /**
@@ -31,83 +31,6 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
      * @param board    is an object of SudokuBoard class
      */
 
-    private void randomFillSector(int sectorNr, SudokuBoard board) {
-        // sectorNr / 3 * 3 - for 0 - 2 : 0; 3 - 5 : 3; 6 - 8 : 6;
-        // sectorNr % 3 * 3 - for 0 : 0; 1 : 3; 2 : 6;
-
-        int begX = (sectorNr / 3) * 3;
-        int begY = (sectorNr % 3) * 3;
-        for (int i = begX; i < begX + 2; i++) {
-            for (int j = begY; j < begY + 2; j++) {
-                if (board.get(i, j) != 0) {
-                    return; // if sector is init, return.
-                }
-            }
-        }
-        Random rand = new Random();
-        int randomVal = rand.nextInt(9) + 1;
-        int randomX = rand.nextInt(3);
-        int randomY = rand.nextInt(3);
-        board.set(begX + randomX, begY + randomY, randomVal);
-        while (board.get(begX + randomX, begY + randomY) != randomVal) {
-            randomVal = rand.nextInt(9) + 1;
-            board.set(begX + randomX, begY + randomY, randomVal);
-        }
-    }
-
-    /**
-     * Method mix rows and columns.
-     */
-
-    private void mixBoard(SudokuBoard board) {
-        Random rand = new Random();
-        int howManyShuffle = rand.nextInt(100);
-        int randomCol;
-        int randomCol2;
-        int randomRow;
-        int randomRow2;
-        for (int i = 0; i < howManyShuffle; i++) {
-            randomCol = rand.nextInt(9);
-            randomRow = rand.nextInt(9);
-            do {
-                randomCol2 = rand.nextInt(3);
-                randomCol2 += (randomCol / 3) * 3;
-            } while (randomCol == randomCol2);
-            do {
-                randomRow2 = rand.nextInt(3);
-                randomRow2 += (randomRow / 3) * 3;
-            } while (randomRow == randomRow2);
-            this.shuffleColumn(randomCol, randomCol2, board);
-            this.shuffleRow(randomRow, randomRow2, board);
-        }
-    }
-
-    /**
-     * Method mix columns.
-     */
-
-    private void shuffleColumn(int col1, int col2, SudokuBoard board) {
-        int prevCol;
-        for (int x = 0; x <= 8; x++) {
-            prevCol = board.get(x, col1);
-            board.set(x, col1, board.get(x, col2));
-            board.set(x, col2, prevCol);
-        }
-    }
-
-    /**
-     * Method mix rows.
-     */
-
-    private void shuffleRow(int row1, int row2, SudokuBoard board) {
-        int prevRow;
-        for (int x = 0; x <= 8; x++) {
-            prevRow = board.get(row1, x);
-            board.set(row1, x, board.get(row2, x));
-            board.set(row2, x, prevRow);
-        }
-    }
-
     /**
      * Solve sudoku filled with some numbers.
      */
@@ -116,7 +39,9 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (board.get(row, col) == 0) {
-                    for (int number = 1; number <= 9; number++) {
+                    List<Integer> range = IntStream.range(1, 10).boxed().collect(Collectors.toList());
+                    Collections.shuffle(range);
+                    for (int number : range) {
                         board.set(row, col, number);
                         if (board.get(row, col) == number) {
                             if (this.solveSudoku(board)) {
