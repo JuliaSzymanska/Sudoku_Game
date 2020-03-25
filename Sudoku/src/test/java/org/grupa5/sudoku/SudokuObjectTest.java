6a9d8dd492702235b10e2607e197d8a8b4ab27f0
224466 Zdrzalik Przemyslaw <224466@edu.p.lodz.pl>
2020-03-25 23:14:45 +0100

    added test for method verify() in sudokuObject

--- Sudoku/src/test/java/org/grupa5/sudoku/SudokuObjectTest.java
+++ Sudoku/src/test/java/org/grupa5/sudoku/SudokuObjectTest.java
@@ -27,4 +27,17 @@ class SudokuObjectTest {
         sudoku2 = new SudokuObject(list2);
         assertNotEquals(sudoku1, sudoku2);
     }
+
+    @Test
+    void verifyTest() {
+    List<SudokuField> list1 = Arrays.asList(new SudokuField[9]);
+    for (int i = 0; i < 9; i++) {
+        list1.set(i, new SudokuField(i + 1));
+    }
+    SudokuObject sudoku1 = new SudokuObject(list1);
+    assertTrue(sudoku1.verify());
+    list1.set(4, new SudokuField(9));
+    sudoku1 = new SudokuObject(list1);
+    assertFalse(sudoku1.verify());
+    }
 }
--- Sudoku/src/test/java/org/grupa5/sudoku/SudokuSolverTest.java
+++ Sudoku/src/test/java/org/grupa5/sudoku/SudokuSolverTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
-import java.util.List;
 
 /**
  * Unit test for simple App. Testing SudokuSolver interface implementations
