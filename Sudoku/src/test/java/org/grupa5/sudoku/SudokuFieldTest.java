577bd53007aa518e53af118182b00243c0cf82d9
224466 Zdrzalik Przemyslaw <224466@edu.p.lodz.pl>
2020-03-25 14:52:30 +0100

    deleted unnecessarry test

--- Sudoku/src/test/java/org/grupa5/sudoku/SudokuFieldTest.java
+++ Sudoku/src/test/java/org/grupa5/sudoku/SudokuFieldTest.java
@@ -8,17 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SudokuFieldTest {
 
-    @Test
-    void getterAndSetterTest() {
-        SudokuBoard sudoku = new SudokuBoard();
-        assertEquals(sudoku.getBoard().get(0).get(0).getFieldValue(), 0);
-        List<List<SudokuField>> lista = sudoku.getBoard();
-        lista.get(0).get(0).setFieldValue(3);
-        assertEquals(sudoku.getBoard().get(0).get(0).getFieldValue(), 0);
-        assertEquals(3, lista.get(0).get(0).getFieldValue());
-        assertThrows(IllegalArgumentException.class, () -> {
-            sudoku.getBoard().get(0).get(0).setFieldValue(10);
-        });
-    }
-
+    
 }
+
+
