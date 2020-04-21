35032408c9466702a817bae2066c5c6317a5eea1
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-04-21 21:11:17 +0200

    Checkstyle in pom

--- Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-    // komentarz PL
+
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
