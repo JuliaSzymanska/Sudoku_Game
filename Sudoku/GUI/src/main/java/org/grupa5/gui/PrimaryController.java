30686ab9728b80756d6235dadb5dc30be0e32547
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-04-28 17:40:03 +0200

    Enum

--- Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
@@ -1,10 +1,26 @@
 package org.grupa5.gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        ComboBox<Level> boxLevel = new ComboBox<>();
+//        boxLevel.setItems( FXCollections.observableArrayList( List.values()));
+    }
+
+    public enum Level{
+        Easy, Medium, High
+    }
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
