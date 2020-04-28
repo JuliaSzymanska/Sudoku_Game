cc6fe368287a513f6b188c642f614f7d289f58b1
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-04-29 00:23:41 +0200

    Dziala

--- Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/PrimaryController.java
@@ -2,57 +2,32 @@ package org.grupa5.gui;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.stage.Stage;
 
 public class PrimaryController implements Initializable {
 
-    @FXML
-    private ComboBox<Level> boxLevel = new ComboBox<>();
 
-    public enum Level{
-        Easy(10),
-        Medium(20),
-        Hard(30);
-
-        private final int number;
-
-        public int getNumber(){
-            return number;
-        }
-
-        Level(int number) {
-            this.number = number;
-        }
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 //        boxLevel.setItems( FXCollections.observableArrayList(Level.values()));
-        boxLevel.getItems().addAll(Level.values());
-        boxLevel.setValue(Level.Easy);
+
 
     }
 
     @FXML
     private void switchToSecondary() throws IOException {
-        System.out.println(Level.Easy.getNumber());
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("secondary.fxml"));
-        Parent rootParent = (Parent) loader.load();
-        SecondaryController second = loader.getController();
-        second.setNumberOfFields(boxLevel.getSelectionModel().getSelectedItem().getNumber());
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootParent));
-        stage.show();
-//        App.setRoot("secondary");
+//        System.out.println(Level.Easy.getNumber());
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("secondary.fxml"));
+//        Parent rootParent = (Parent) loader.load();
+//        SecondaryController second = loader.getController();
+//        second.setNumberOfFields(boxLevel.getSelectionModel().getSelectedItem().getNumber());
+//        Stage stage = new Stage();
+//        stage.setScene(new Scene(rootParent));
+//        stage.show();
+        App.setRoot("secondary");
     }
 }
--- Sudoku/GUI/src/main/java/org/grupa5/gui/SecondaryController.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/SecondaryController.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -14,11 +17,35 @@ import org.grupa5.sudoku.SudokuBoard;
 
 public class SecondaryController implements Initializable {
 
-    private int numberOfFilds;
+    private int numberOfFields;
     private SudokuBoard sudokuBoard = new SudokuBoard();
+    private boolean flag = true;
+
+
+    @FXML
+    private ComboBox<Level> boxLevel = new ComboBox<>();
+
+    public enum Level{
+        Easy(42),
+        Medium(54),
+        Hard(60);
+
+        private final int number;
+
+        public int getNumber(){
+            return number;
+        }
+
+        Level(int number) {
+            this.number = number;
+        }
+    }
 
     @FXML
     private GridPane grid1;
+
+    @FXML
+    private Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -26,42 +53,47 @@ public class SecondaryController implements Initializable {
     }
 
     public void setNumberOfFields(int number){
-        this.numberOfFilds = number;
+        this.numberOfFields = number;
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void startGame() {
+        if(!flag){
+            try {
+                this.switchToPrimary();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            flag = true;
+            return;
+        }
+        flag = false;
+        secondaryButton.setText("END GAME");
+        this.numberOfFields = boxLevel.getSelectionModel().getSelectedItem().getNumber();
         sudokuBoard.solveGame();
-        this.numberOfFilds = 50;
-        System.out.println(this.numberOfFilds);
-        sudokuBoard.removeFields(this.numberOfFilds);
+        sudokuBoard.removeFields(this.numberOfFields);
         int numRows = grid1.getRowCount();
         int numCols = grid1.getColumnCount();
 
         // Wypełnienie gridpane polami tekstowymi
         for(int i = 0; i < numRows; i++) {
-            for(int j = 0; j < numCols; j++) {
-                if(i == 0 && j == 0) {
-                    grid1.add(new Label("X"), 0,0);
-                }
-                else if (i != 0 && j == 0) {
+            for (int j = 0; j < numCols; j++) {
+                if (i == 0 && j == 0) {
+                    grid1.add(new Label("X"), 0, 0);
+                } else if (i != 0 && j == 0) {
                     grid1.add(new Label("0" + Integer.toString(i)), i, j);
-                }
-                else if (i == 0) {
+                } else if (i == 0) {
                     grid1.add(new Label(Character.toString((char) (64 + j))), i, j);
-                }
-                else {
+                } else {
                     int intToAdd = sudokuBoard.get(j - 1, i - 1);
-                    if(sudokuBoard.get(j - 1, i - 1) == 0){
-                    grid1.add(new TextField(Integer.toString(intToAdd)), i, j);}
-                    else {
+                    if (sudokuBoard.get(j - 1, i - 1) == 0) {
+                        grid1.add(new TextField(Integer.toString(intToAdd)), i, j);
+                    } else {
                         grid1.add(new Label(Integer.toString(intToAdd)), i, j);
                     }
                 }
 
             }
         }
-
         // CENTROWANIE
         for( Object i : grid1.getChildren()) {
             if(i instanceof TextField) {
@@ -72,6 +104,13 @@ public class SecondaryController implements Initializable {
                 ((Label) i).setAlignment(Pos.CENTER);
             }
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        boxLevel.setItems( FXCollections.observableArrayList(Level.values()));
+//        boxLevel.getItems().addAll(Level.values());
+        boxLevel.setValue(Level.Easy);
     }
 
 }
