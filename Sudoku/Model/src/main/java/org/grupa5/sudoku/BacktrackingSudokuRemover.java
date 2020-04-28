055ad15ca1fc701f1df182116334e32dbf56b136
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-04-28 22:03:53 +0200

    Solver

--- Sudoku/Model/src/main/java/org/grupa5/sudoku/BacktrackingSudokuRemover.java
+++ Sudoku/Model/src/main/java/org/grupa5/sudoku/BacktrackingSudokuRemover.java
@@ -6,26 +6,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class BacktrackingSudokuRemover implements SudokuRemover{
+public class BacktrackingSudokuRemover implements SudokuRemover {
 
     // TODO: nie wiem czy tego wszystkiego potrzebuje
     private List<SudokuField> allFields = new ArrayList<SudokuField>();
     private SudokuBoard sudokuBoard;
     private int numOfFieldsToRemove;
-    private List<List<SudokuField>> copyBoard;
+//    private List<List<SudokuField>> copyBoard;
+    private SudokuBoard copyBoard;
     private int curreentNumOfPossibleBoards;
 
     // TODO: nie wiem czy w funkcji remove nie wystarczy tego podać i mądrze to zrobić
-    public BacktrackingSudokuRemover(SudokuBoard board, int numOfFieldsToRemove) {
+    public BacktrackingSudokuRemover(SudokuBoard board, int numOfFieldsToRemove) throws CloneNotSupportedException {
         this.sudokuBoard = board;
         this.numOfFieldsToRemove = numOfFieldsToRemove;
-        this.copyBoard = board.getBoard();
-        for(List<SudokuField> var : this.copyBoard) {
+        this.copyBoard =  this.sudokuBoard.clone();
+        for (List<SudokuField> var : this.copyBoard.getBoard()) {
             allFields.addAll(var);
         }
         Collections.shuffle(this.allFields);
         remove();
-
     }
 
     //TODO : totalny work in progress.
@@ -39,12 +39,13 @@ public class BacktrackingSudokuRemover implements SudokuRemover{
     // Wydaje się już teraz proste pod założeniem że to co tam mam ten 'solve sudoku' to działq
     @Override
     public void remove() {
-        solveSudoku(this.sudokuBoard);
+        checkNumbOfCombinations(this.copyBoard);
         System.out.println(curreentNumOfPossibleBoards);
+//        setOriginalBoardToCopy();
     }
 
     // TODO: to jest kopia z solvera poza miejscem gddzie jest drugie todo
-    private boolean solveSudoku(SudokuBoard board) {
+    private boolean checkNumbOfCombinations(SudokuBoard board) {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (board.get(row, col) == 0) {
@@ -54,7 +55,7 @@ public class BacktrackingSudokuRemover implements SudokuRemover{
                     for (int number : range) {
                         board.set(row, col, number);
                         if (board.get(row, col) == number) {
-                            if (this.solveSudoku(board)) {
+                            if (this.checkNumbOfCombinations(board)) {
                                 return true;
                             } else {
                                 board.set(row, col, 0);
@@ -72,11 +73,17 @@ public class BacktrackingSudokuRemover implements SudokuRemover{
     }
 
     private void setOriginalBoardToCopy() {
-
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (this.copyBoard.get(row, col) == 0){
+                    this.sudokuBoard.resetField(row, col);
+                }
+            }
+        }
     }
 
-//    public static void main(String[] args) {
-//        SudokuBoard board = new SudokuBoard();
+    public static void main(String[] args) throws CloneNotSupportedException {
+        SudokuBoard board = new SudokuBoard();
 //        board.solveGame();
 //        board.resetField(5, 5);
 //        board.resetField(5, 4);
@@ -96,7 +103,50 @@ public class BacktrackingSudokuRemover implements SudokuRemover{
 //        board.resetField(4, 6);
 //        board.resetField(4, 7);
 //        board.resetField(4, 8);
-//        System.out.println(board);
-//        SudokuRemover remover = new BacktrackingSudokuRemover(board, 0);
-//    }
+//        board.resetField(3, 5);
+//        board.resetField(3, 4);
+//        board.resetField(3, 3);
+//        board.resetField(3, 2);
+//        board.resetField(3, 1);
+//        board.resetField(3, 0);
+//        board.resetField(3, 6);
+//        board.resetField(3, 7);
+//        board.resetField(3, 8);
+
+//        board.set(0,0,7);
+//        board.set(0,2,5);
+//        board.set(1,3,7);
+//        board.set(1,5,2);
+//        board.set(1,6,5);
+//        board.set(1,7,9);
+//        board.set(2,1,8);
+//        board.set(2,6,3);
+//        board.set(2,8,7);
+//        board.set(3,0,1);
+//        board.set(3,4,5);
+//        board.set(3,6,4);
+//        board.set(3,7,2);
+//        board.set(4,1,6);
+//        board.set(4,3,2);
+//        board.set(4,4,7);
+//        board.set(4,5,8);
+//        board.set(4,7,3);
+//        board.set(5,1,2);
+//        board.set(5,2,9);
+//        board.set(5,4,6);
+//        board.set(5,8,8);
+//        board.set(6,0,8);
+//        board.set(6,2,7);
+//        board.set(6,7,1);
+//        board.set(7,1,4);
+//        board.set(7,2,3);
+//        board.set(7,3,6);
+//        board.set(7,5,5);
+//        board.set(8,6,9);
+//        board.set(8,8,5);
+
+        System.out.println(board);
+        SudokuRemover remover = new BacktrackingSudokuRemover(board, 20);
+        System.out.println(board);
+    }
 }
