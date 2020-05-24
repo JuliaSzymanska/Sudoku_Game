f77a91d045c001cfd4cf9a0f9edb874ffceb11a6
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-05-24 16:44:20 +0200

    Grid

--- Sudoku/GUI/src/main/java/org/grupa5/gui/App.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
