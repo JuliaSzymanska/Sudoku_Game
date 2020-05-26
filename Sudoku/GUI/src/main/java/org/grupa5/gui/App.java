a9f0507d907847487939513e9d73ef1c94f49680
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-05-26 20:19:01 +0200

    todo

--- Sudoku/GUI/src/main/java/org/grupa5/gui/App.java
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/App.java
@@ -29,9 +29,10 @@ public class App extends Application {
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setResizable(false);
         // TODO: 26.05.2020 Pani artystko, ustaw jak ci sie lepiej wydaje
-//        stage.initStyle(StageStyle.UNDECORATED);
+        //  stage.initStyle(StageStyle.UNDECORATED);
         // TODO: to jest ikonka ta windowsowa, takie co sie pokazuja na pasku na dole i na pasku aplikacji po lewej
         //  pobrałem z neta, jeśli ci się chce możesz zrobić własną
+        //  Google Sudoku Icon dla inspiracji
         stage.getIcons().add(new Image(App.class.getResourceAsStream( "IconZNeta.png" )));
 
         stage.setScene(scene);
