23dfd8d8d4027adc608cf7665ce3fbc5eb9cd31a
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-05-26 17:29:08 +0200

    being own convertert

---
+++ Sudoku/GUI/src/main/java/org/grupa5/gui/SudokuNumberStringConverter.java
@@ -0,0 +1,9 @@
+package org.grupa5.gui;
+
+import javafx.util.converter.NumberStringConverter;
+
+public class SudokuNumberStringConverter extends NumberStringConverter {
+    public SudokuNumberStringConverter() {
+        super();
+    }
+}
